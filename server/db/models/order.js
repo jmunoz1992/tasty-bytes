6c4a06292202540423897374e571f0b9b72fa783
Jaime Syvino <35039627+jsyvino@users.noreply.github.com>
2018-04-17 16:21:34 -0500

    Update order.js

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -2,7 +2,6 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Order = db.define('order', {
-    // ONLY NEED THIS IF WE DO THE BELONGS TO MANY ASSOCIATIONS
   id: {
     type: Sequelize.INTEGER,
     autoIncrement: true,
