1969276938621376a019553895abba98ba3ee332
Jvwalsh <jvwalsh@icloud.com>
2018-04-17 15:30:46 -0500

    Added id PK for Order model.

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -2,6 +2,13 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Order = db.define('order', {
+    // ONLY NEED THIS IF WE DO THE BELONGS TO MANY ASSOCIATIONS
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false
+  },
   shipped: {
     type: Sequelize.DATE,
     allowNull: true,
