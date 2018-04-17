afa69b17ec520d884ecddff1a4f77ad8557ad615
Jvwalsh <jvwalsh@icloud.com>
2018-04-17 14:07:53 -0500

    completed Order model

---
+++ server/db/models/order.js
@@ -0,0 +1,11 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Order = db.define('order', {
+  status: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+})
+
+module.exports = Order;
