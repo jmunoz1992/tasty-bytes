92c3f6493b8cf582ef8e0ea56ae7a0daa1ab85a1
jmunoz1992 <jasmine.esplago.munoz@gmail.com>
2018-04-17 10:57:43 -0500

    started products model

---
+++ server/db/models/products.js
@@ -0,0 +1,54 @@
+const crypto = require('crypto')
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Products = db.define('products', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  shortDescription: {
+    type: Sequelize.STRING,
+    validate: {
+      notEmpty: true
+    }
+  },
+  fullDescription: {
+    type: Sequelize.TEXT,
+    validate: {
+      notEmpty: true
+    }
+  },
+  price: {
+    type: Sequelize.FLOAT,
+    allowNull: false
+  },
+  inventoryQty: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0,
+    allowNull: false,
+    validate: {
+      min: 0
+    }
+  },
+  // will be calculated from all ratings from all reviews for this product
+  ratingsAvg: {
+    type: Sequelize.FLOAT,
+    defaultValue: 0.0
+  },
+  // will be calculated from pkgWt / price
+  unitCost: {
+    type: Sequelize.FLOAT,
+    defaultValue: 0.0
+  },
+  images: {
+    type: Sequelize.ARRAY(Sequelize.STRING),
+    defaultValue: ['http://fillmurray.com/140/200']
+  },
+  pkgWt: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  }
+});
+
+module.exports = Products;
