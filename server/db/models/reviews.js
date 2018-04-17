9749127dd2f003a17a045d8cfaa29429787e066e
Jaime Syvino <jsyvino@gmail.com>
2018-04-17 13:22:38 -0500

    added the PK id to reviews and added test specs

--- server/db/models/reviews.js
+++ server/db/models/reviews.js
@@ -2,6 +2,12 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Review = db.define('review', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false
+  },
   title: {
     type: Sequelize.STRING,
     allowNull: false
