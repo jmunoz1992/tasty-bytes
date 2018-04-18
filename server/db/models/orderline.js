57a445ae297379fec95e371b0a751c354ec77a30
Jvwalsh <jvwalsh@icloud.com>
2018-04-18 18:48:57 -0500

    need to update my branch and commit current changes

--- server/db/models/orderline.js
+++ server/db/models/orderline.js
@@ -2,11 +2,17 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Orderline = db.define('orderline', {
+    id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false
+  },
   qty: {
     type: Sequelize.INTEGER,
     allowNull: false
   },
-  price: {
+  priceCents: {
     type: Sequelize.DECIMAL(5, 2), // initially setting for upper price limit to be 999.99
     allowNull: false
   },
