8956c7435d8b841199801fb9d436cbd17ea524e6
Jaime Syvino <jsyvino@gmail.com>
2018-04-20 15:39:15 -0500

    new users can now sign up

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -1,17 +1,32 @@
 import React from 'react'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
-import {auth} from '../store'
+import { auth } from '../store'
 
 /**
  * COMPONENT
  */
 const AuthForm = (props) => {
-  const {name, displayName, handleSubmit, error} = props
+  const { name, displayName, handleSubmit, error } = props
 
   return (
     <div>
       <form onSubmit={handleSubmit} name={name}>
+        {
+          displayName === "Sign Up" ?
+            <div>
+              <div>
+                <label htmlFor="username"><small>Username</small></label>
+                <input name="username" type="text" />
+              </div>
+              <div>
+                <label htmlFor="fullName"><small>Full Name</small></label>
+                <input name="fullName" type="text" />
+              </div>
+            </div>
+            :
+            <div />
+        }
         <div>
           <label htmlFor="email"><small>Email</small></label>
           <input name="email" type="text" />
@@ -55,12 +70,14 @@ const mapSignup = (state) => {
 
 const mapDispatch = (dispatch) => {
   return {
-    handleSubmit (evt) {
+    handleSubmit(evt) {
       evt.preventDefault()
       const formName = evt.target.name
       const email = evt.target.email.value
       const password = evt.target.password.value
-      dispatch(auth(email, password, formName))
+      const username = evt.target.username.value
+      const name = evt.target.fullName.value
+      dispatch(auth(email, password, username, name, formName))
     }
   }
 }
