42316789058b26cbcde9c2761c5f3d683518a241
Jvwalsh <31374355+Jvwalsh@users.noreply.github.com>
2018-04-20 11:55:05 -0500

    Update orders.js

--- client/store/orders.js
+++ client/store/orders.js
@@ -1,25 +1,14 @@
 import axios from 'axios'
 import history from '../history'
 
-/**
- * ACTION TYPES
- */
 const GET_ORDERS = 'GET_ORDERS'
 const UPDATE_ORDER = 'UPDATE_ORDER'
-// const REMOVE_ORDER = 'REMOVE_ORDER'
 
-/**
- * INITIAL STATE
- */
 const defaultOrder = []
 
-/**
- * ACTION CREATORS
- */
 const getOrders = orders => ({type: GET_ORDERS, orders})
 const updateOrder = order => ({type: UPDATE_ORDER, order})
 
-// const removeOrder = () => ({type: REMOVE_ORDER})
 
 /**
  * THUNK CREATORS
@@ -32,14 +21,6 @@ export const fetchOrders = () =>
     )
       .catch(err => console.log(err))
 
-// export const orderShipped = (orderId) =>
-// dispatch => {  
-//   return axios.put(`/api/admin/orders/${orderId}`)
-//     .then(res => {
-//       dispatch(updateOrder(res.data))}
-//   )
-//     .catch(err => console.log(err))
-// }
 export function orderShipped(id, updates) {
   console.log('called thunk', id)
   return function thunk(dispatch) {
@@ -55,29 +36,7 @@ export function orderShipped(id, updates) {
   };
 }
 
-// export const auth = (email, password, method) =>
-//   dispatch =>
-//     axios.post(`/auth/${method}`, { email, password })
-//       .then(res => {
-//         dispatch(getOrders(res.data))
-//         history.push('/home')
-//       }, authError => { // rare example: a good use case for parallel (non-catch) error handler
-//         dispatch(getOrders({error: authError}))
-//       })
-//       .catch(dispatchOrHistoryErr => console.error(dispatchOrHistoryErr))
 
-// export const logout = () =>
-//   dispatch =>
-//     axios.post('/auth/logout')
-//       .then(_ => {
-//         dispatch(removeOrder())
-//         history.push('/login')
-//       })
-//       .catch(err => console.log(err))
-
-/**
- * REDUCER
- */
 export default function (state = defaultOrder, action) {
   switch (action.type) {
     case GET_ORDERS:
