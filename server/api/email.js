5d5beac05917d5fc234e4ce4bd4bbae043a896ac
Ricky Li <ricky_0321@hotmail.com>
2018-04-24 21:33:01 -0500

    api route for shipped email

--- server/api/email.js
+++ server/api/email.js
@@ -7,7 +7,7 @@ const nodemailer = require('nodemailer');
 
 router.get('/', (req, res) => {
   res.send('hello');
-})
+});
 
 router.post('/sendConformation', (req, res, next) => {
   console.log(req.body);
@@ -30,7 +30,44 @@ router.post('/sendConformation', (req, res, next) => {
         to: req.body.email, // list of receivers
         subject: 'Order Conformation from Tasty Bytes', // Subject line
         text: ' ', // plain text body
-        html: '<center><h4>Your Order has been Created. Please wait in your doorway for your sweets!</h4><img src="https://img1.goodfon.com/wallpaper/big/2/97/taynaya-zhizn-domashnih.jpg" /></center>' // html body
+        html: '<center><h4>Your Order has been Created. Please wait in your doorway for your sweets!</h4><br /><img src="https://img1.goodfon.com/wallpaper/big/2/97/taynaya-zhizn-domashnih.jpg" /></center>' // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        // Preview only available when sending through an Ethereal account
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+        res.status(200);
+    });
+});
+
+router.post('/sendShipped', (req, res, next) => {
+  console.log(req.body);
+
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+           user: 'tastybytestest@gmail.com',
+           pass: 'RJJJ1234'
+       },
+    tls: {
+      rejectUnauthorized: false
+    }
+    });
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Tasty Bytes" <tastybytestest@gmail.com>', // sender address
+        to: req.body.email, // list of receivers
+        subject: 'Order has been shipped from Tasty Bytes', // Subject line
+        text: ' ', // plain text body
+        html: '<center><h4>Your Order has been Shipped</h4><br /><img src="https://media.giphy.com/media/3o6Ztoo0FQBBJwpl5u/giphy.gif" /></center>' // html body
     };
 
     // send mail with defined transport object
